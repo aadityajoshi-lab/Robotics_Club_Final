9edc84b6bf8ffc95a63bc4ad56ff2997b536a341
aadityajoshi-lab <itsmeaadityajoshi.com>
2026-06-14 08:59:45 +0545

    domain updated-2

--- robotics_club/settings.py
+++ robotics_club/settings.py
@@ -31,7 +31,7 @@ DEBUG = os.environ.get("DEBUG", "False") == "True"
 ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
-    ".onrender.com"
+    ".onrender.com",
     'roboticsclub.kecktm.edu.np',         
     'www.roboticsclub.kecktm.edu.np',
 ]
